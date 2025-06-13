968f59d795400b41e242d09cc32402333083087f
radwa elsayed <radwa elsayed@gmail.com>
2025-06-13 18:28:54 +0300

    auth

--- server-side/src/routes/auth.js
+++ server-side/src/routes/auth.js
@@ -1,8 +1,15 @@
+
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/auth/authController');
+const validationMW = require('../middleware/validationMW'); 
+const { signupSchema, loginSchema } = require('../utils/authSchemas'); 
 
-router.get('/', (req, res) => {
-  res.send('Auth route works!');
-});
+router.post('/signup', validationMW(signupSchema), authController.signup);
+router.post('/login', validationMW(loginSchema), authController.login);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
+router.get("/google", (req, res) => res.send("Google Auth Init Placeholder"));
+router.get('/google/callback', authController.google);
 
 module.exports = router;
