a761c59e4f9b9fc466344bf17214ce5518a1a34f
mariemhawary <mariemhawary843@gmail.com>
2025-06-13 15:31:26 +0300

    add blog controller

--- server-side/src/routes/admin.js
+++ server-side/src/routes/admin.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin/userController");
+const blogController = require("../controllers/admin/blogController");
+
 router.get("/", (req, res) => {
   res.send("Admin route works!");
 });
@@ -9,5 +11,10 @@ router.get("/doctors", adminController.getAllDoctors);
 router.post("/doctors", adminController.addDoctor);
 router.delete("/doctors/:doctorId", adminController.deleteDoctor);
 router.get("/patients", adminController.getAllPatients);
+
+router.post("/blog", blogController.addBlog);
+router.get("/blog", blogController.getAllBlogs);
+router.get("/blog/:id", blogController.getBlogById);
+router.delete("/blog/:id", blogController.deleteBlog);
 
 module.exports = router;
