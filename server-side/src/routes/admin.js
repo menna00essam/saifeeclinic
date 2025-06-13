04630fa128c87382073eec16d0d8db22b171b80b
mariemhawary <mariemhawary843@gmail.com>
2025-06-13 19:28:33 +0300

    add auth middleware and protect

--- server-side/src/routes/admin.js
+++ server-side/src/routes/admin.js
@@ -3,19 +3,30 @@ const router = express.Router();
 const adminController = require("../controllers/admin/userController");
 const blogController = require("../controllers/admin/blogController");
 const allowedTo = require("../middleware/roleAuth");
+const auth = require("../middleware/protectMW.js");
 
 router.get("/", (req, res) => {
   res.send("Admin route works!");
 });
 
-router.get("/doctors", allowedTo('admin'),adminController.getAllDoctors);
-router.post("/doctors",allowedTo('admin'), adminController.addDoctor);
-router.delete("/doctors/:doctorId",allowedTo('admin'), adminController.deleteDoctor);
-router.get("/patients", allowedTo('admin'),adminController.getAllPatients);
+router.get("/doctors", auth, allowedTo("admin"), adminController.getAllDoctors);
+router.post("/doctors", auth, allowedTo("admin"), adminController.addDoctor);
+router.delete(
+  "/doctors/:doctorId",
+  auth,
+  allowedTo("admin"),
+  adminController.deleteDoctor
+);
+router.get(
+  "/patients",
+  auth,
+  allowedTo("admin"),
+  adminController.getAllPatients
+);
 
-router.post("/blog",allowedTo('admin'), blogController.addBlog);
-router.get("/blog",allowedTo('admin'), blogController.getAllBlogs);
-router.get("/blog/:id",allowedTo('admin'), blogController.getBlogById);
-router.delete("/blog/:id",allowedTo('admin'), blogController.deleteBlog);
+router.post("/blog", auth, allowedTo("admin"), blogController.addBlog);
+router.get("/blog", auth, allowedTo("admin"), blogController.getAllBlogs);
+router.get("/blog/:id", auth, allowedTo("admin"), blogController.getBlogById);
+router.delete("/blog/:id", auth, allowedTo("admin"), blogController.deleteBlog);
 
 module.exports = router;
