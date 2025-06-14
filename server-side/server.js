bad579f159d7eb303da4726edfa8de8d183fbc95
Asm-123 <asmaa2470a@gmail.com>
2025-06-14 03:16:54 +0300

    doctors

--- server-side/server.js
+++ server-side/server.js
@@ -1,22 +1,19 @@
-const app = require('./src/app');
-const { connectDB } = require('./src/config/database');
-const EmailJobs = require('./src/jobs/emailJobs');
+const app = require("./src/app");
+const { connectDB } = require("./src/config/database");
+const EmailJobs = require("./src/jobs/emailJobs");
 
-require('./src/config/env');
-const errorHandling = require('./src/middleware/errorHandling');
-
+require("./src/config/env");
+const errorHandling = require("./src/middleware/errorHandling");
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 EmailJobs.startAllJobs();
-console.log('Server started with notification jobs running...');
-console.log('EMAIL_USER:', process.env.EMAIL_USER);
-console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
+console.log("Server started with notification jobs running...");
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
 
 app.use(errorHandling);
-app.use("/api/doctors", require("./src/routes/doctor.js")); 
-
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
